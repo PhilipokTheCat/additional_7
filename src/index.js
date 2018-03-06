9cc6116fd1e3f3cc0410311099285cf66aa89223
unknown <philipok4596@gmail.com>
2018-03-07 00:29:42 +0300

    additional_7/feature: impelement sudoku solving function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+  let zeroI, zeroJ, breakProp = false, naturalNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  for (let i = 0; i < 9; i++){
+    for (let j = 0; j < 9; j++){
+      if (matrix[i][j] === 0){
+        breakProp = true;
+        zeroI = i;
+        zeroJ = j;
+        break;
+      }
+    }
+    if (breakProp) break;
+  }
+  for (let i = 0; i < 9; i++) {
+    let hIndex = naturalNumbers.indexOf(matrix[zeroI][i]), vIndex = naturalNumbers.indexOf(matrix[i][zeroI]);
+    if (matrix[zeroI][i] !== 0){
+      if (hIndex > -1) naturalNumbers.splice(hIndex, 1);
+      if (vIndex > -1) naturalNumbers.splice(vIndex, 1);
+    }
+  }
 }
